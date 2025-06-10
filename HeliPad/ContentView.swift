22f06e7db59ae632f03fe236b5664cce0c6898bf
SunnyFlops <arav.prasad@outlook.com>
2025-06-10 19:54:31 +1200

    acronym shortcut

--- HeliPad/ContentView.swift
+++ HeliPad/ContentView.swift
@@ -55,10 +55,26 @@ struct ContentView: View {
     var filteredApps: [AppInfo] {
         if searchText.isEmpty {
             return fetcher.apps
-        } else {
-            return fetcher.apps.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        }
+
+        let query = searchText.lowercased()
+
+        return fetcher.apps.filter { app in
+            let name = app.name.lowercased()
+
+            if name.contains(query) {
+                return true
+            }
+
+            let acronym = app.name.compactMap { char -> String? in
+                let s = String(char)
+                return s.uppercased() == s && s.rangeOfCharacter(from: .letters) != nil ? s : nil
+            }.joined().lowercased()
+
+            return acronym.contains(query)
         }
     }
+
 }
 
 struct AppLauncherButton: View {
