11769115d0cee194d2305bad11a941cb8c8c9618
SunnyFlops <arav.prasad@outlook.com>
2025-06-11 15:28:31 +1200

    drag UI improvment

--- HeliPad/Components/AppLauncherButton.swift
+++ HeliPad/Components/AppLauncherButton.swift
@@ -11,44 +11,45 @@ import UniformTypeIdentifiers
 
 struct AppLauncherButton: View {
     let app: AppInfo
-    @State private var isHovering = false
     @Binding var draggedItem: AppInfo?
     @EnvironmentObject var fetcher: AppFetcher
+    @State private var isHovering = false
 
     var body: some View {
-        Button {
+        VStack(spacing: 4) {
+            Image(nsImage: app.icon ?? NSImage())
+                .resizable()
+                .frame(width: 64, height: 64)
+            Text(app.name)
+                .font(.caption)
+                .lineLimit(1)
+                .multilineTextAlignment(.center)
+        }
+        .frame(width: 80)
+        .scaleEffect(isHovering ? 1.15 : 1.0)
+        .animation(.interpolatingSpring(stiffness: 300, damping: 15), value: isHovering)
+        .onHover { isHovering = $0 }
+        .onTapGesture {
             if NSEvent.modifierFlags.contains(.command) {
                 NSWorkspace.shared.activateFileViewerSelecting([app.url])
             } else {
                 NSWorkspace.shared.open(app.url)
             }
-        } label: {
-            VStack {
-                if let icon = app.icon {
-                    Image(nsImage: icon)
-                        .resizable()
-                        .frame(width: 64, height: 64)
-                }
-                Text(app.name)
-                    .font(.caption)
-                    .lineLimit(1)
-                    .multilineTextAlignment(.center)
-            }
-            .frame(width: 80)
-            .scaleEffect(isHovering ? 1.15 : 1.0)
-            .animation(.interpolatingSpring(stiffness: 300, damping: 15), value: isHovering)
-            .onHover { hovering in
-                isHovering = hovering
-            }
         }
-        .buttonStyle(PlainButtonStyle())
-        .onDrag {
-            self.draggedItem = app
+        .onDrag({
+            draggedItem = app
             return NSItemProvider(object: app.identifier as NSString)
-        }
-        .onDrop(of: [.text],
-                delegate: AppDropDelegate(item: app,
-                                          fetcher: fetcher,
-                                          draggedItem: $draggedItem))
+        }, preview: {
+            Image(nsImage: app.icon ?? NSImage())
+                .resizable()
+                .frame(width: 64, height: 64)
+                .shadow(radius: 4)
+        })
+        .onDrop(of: [.text], delegate:
+            AppDropDelegate(item: app,
+                            fetcher: fetcher,
+                            draggedItem: $draggedItem)
+        )
     }
 }
+
